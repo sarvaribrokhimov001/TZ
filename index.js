90ed034c7037eb841746955c1d977d6862ff84db
ibrokhimovnba <ibrokhimovnba@gmail.com>
2026-01-08 20:36:08 +0500

    TZ

--- index.js
+++ index.js
@@ -1,53 +1,4 @@
-// const api = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether,binancecoin&vs_currencies=usd";
-// const mainContainerSections = document.querySelector('.main__container__sections');
-
-// fetch(api)
-// .then(res => res.json())
-// .then((data) => {
-//     showData(data);
-// });
-
-// function showData(data) {
-//     data.map((item) => {
-//         const {} = item;
-//         // console.log(flags);
-//         console.log(data);
-        
-//     mainContainerSections.innerHTML += `
-    
-//                   <div>
-//                     <div class="main__container__section">
-//                         <p class="main__container__section__text"> binancecoin </p>
-//                         <p class="main__container__section__price"> $919,79 </p>
-//                     </div> <!-- main__container__section -->
-
-//                       <div class="main__container__section">
-//                         <p class="main__container__section__text"> bitcoin </p>
-//                         <p class="main__container__section__price"> $94 280 </p>
-//                     </div> <!-- main__container__section -->
-
-//                       <div class="main__container__section">
-//                         <p class="main__container__section__text"> ethereum </p>
-//                         <p class="main__container__section__price"> $3 298,57 </p>
-//                     </div> <!-- main__container__section -->
-
-//                       <div class="main__container__section">
-//                         <p class="main__container__section__text"> tether </p>
-//                         <p class="main__container__section__price"> $1 </p>
-//                     </div> <!-- main__container__section -->
-//                   </div>
-//     `
-//     });
-//  };
-
-
-
-
-
-
-
-
- Toastify({
+Toastify({
     text: "Dasturimizga xush kelibsiz !",
     duration: 3500,
     gravity: "top",
@@ -78,7 +29,7 @@
 const api = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether,binancecoin&vs_currencies=usd";
 const mainContainerSections = document.querySelector(".main__container__sections");
 
-fetch(api)
+fetch(`${api}`)
   .then(res => res.json())
   .then(data => {
     console.log(data);
